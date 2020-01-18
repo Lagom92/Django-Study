dd097b17406376e0e95833d2b04ad46f96a0abd9
Lagom92 <jyyt0147@gmail.com>
2020-01-18 22:47:14 +0900

    Create update default

--- board/urls.py
+++ board/urls.py
@@ -7,4 +7,6 @@ app_name = 'board'
 urlpatterns = [
     path('listFBV/', views.listFBV, name="list"),
     path('listCBV/', views.listCBV.as_view()),
+    path('listCBV2/', views.listCBV2.as_view()),
+    path('listCBV3/', views.listCBV3.as_view()),
 ]
--- board/views.py
+++ board/views.py
@@ -9,10 +9,23 @@ from .models import Paper
 #     queryset = Hobby.objects.all()
 #     serializer_class = HobbySerializer
 
-
+# FBV list
 def listFBV(request):
     papers = Paper.objects.all()
     return render(request, 'board/index.html', {'papers':papers})
 
+# CBV list
 class listCBV(ListView):
     model = Paper
+
+# default 설정 변경 - CBV list
+class listCBV2(ListView):
+    template_name = 'board/index.html'
+    context_object_name = 'papers'
+    model = Paper
+
+# 컨텍스트 오버라이딩
+class listCBV3(ListView):
+    def get_queryset(self):
+        return Paper.objects.all()
+
