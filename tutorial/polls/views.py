4424d5bb78fec3989249f34950083133dd765d59
Lagom92 <jyyt0147@gmail.com>
2020-03-13 22:09:34 +0900

    Edit index

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, World !")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    res = ', '.join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, id):
+    return HttpResponse(f"{id}번 Question")
+
+def results(request, id):
+    response = "Results of Question"
+    return HttpResponse(response)
+
+def vote(request, id):
+    return HttpResponse(f"Vote {id}번 question")
